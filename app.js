245d0b562626ffd5dbb7ddb3128c0330743bde3c
Tatiana Mraz <tatiana.mrazova@icloud.com>
2023-05-24 18:16:42 +0200

    FORM part1: push data from browser form to .json array

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const fs = require('fs'); //to open files
 const path = require('path');
 const express = require('express');
 const app = express();
@@ -6,6 +7,8 @@ const port = 3000;
 
 //access to static css, js files
 app.use(express.static('public'));
+//post data: look to incomming request and extract incomming data
+app.use(express.urlencoded({ extended: false }));
 
 //view dynamically created content
 app.set('views', path.join(__dirname, 'views')); 
@@ -30,9 +33,24 @@ app.get('/recommend', (req, res) => {
     res.sendFile(htmlFilepath);
 });
 
+// post form data
+app.post('/recommend', (req, res) => {
+    const restaurant = req.body;
+    const filePath = path.join(__dirname, 'data', 'restaurants.json');
+    
+    const fileData = fs.readFileSync(filePath); //read data from restaurants.json
+    const storedRestaurants = JSON.parse(fileData); //parse data from array from restaurants.json
+    
+    storedRestaurants.push(restaurant); //push data from html form to array to restaurants.json
+    fs.writeFileSync(filePath, JSON.stringify(storedRestaurants)); //save to restaurants.json
+    
+    res.redirect('/confirm'); //save data to confirm.html
+});
+
 app.get('/restaurants', (req, res) => {
     const htmlFilepath = path.join(__dirname, 'views', 'restaurants.html');
     res.sendFile(htmlFilepath);
 });
+
 
 app.listen(port, () => {});
